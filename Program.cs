b9eed5c1fa1f27dcd90789684fa74238875f2f3d
fishman123456 <den9152@yandex.ru>
2022-10-21 00:21:12 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+namespace SimpleProject
+{
+    class Example
+    {
+        int _num;
+        public int Num
+        {
+            get { return _num; }
+            set { _num = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example example = new Example();
+            Write("Введите целое число:");
+            example.Num = int.Parse(ReadLine()); // set
+            
+        Write("Вы ввели:");
+            WriteLine(example.Num); // get
+        }
+    }
+}
