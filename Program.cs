0157880f71e1d38dd3cc4a1f1f737a2ff086e562
denis <106389581+fishman123456@users.noreply.github.com>
2022-10-21 10:23:36 +0700

    доделал
    Итого дома два часа, на работе 2 часа

--- Program.cs
+++ Program.cs
@@ -1,31 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Создать класс сотрудники (имя, возраст, должность, зарплата)
-// Создать геттреры и сеттеры с проверкой.
-// Заполнить обьект с клавиатуры и вывести на экран
-//staff - сотрудники
-//_name _age    
+﻿//  See https://aka.ms/new-console-template for more information
+//  Создать класс сотрудники (имя, возраст, должность, зарплата)
+//  Создать геттреры и сеттеры с проверкой.
+//  Заполнить обьект с клавиатуры и вывести на экран
+//  Staff - сотрудники
+//  _name - имя 
+//  _age - возраст
+//  _post - должность
+//  _salary - зарплата
+
+using System.Xml.Linq;
+
 using static System.Console;
 namespace SimpleProject
 {
-    class Example
+    class Staff
     {
-        int _num;
-        public int Num
+        private String _name; // поле имя
+        private int _age; // поле возраст
+        private string _post; // поле должность
+        private int _salary; // поле зарплата
+
+        public Staff()
         {
-            get { return _num; }
-            set { _num = value; }
+            string _name;
+            int _age;
+            string _post;
+            int _salary;
         }
+        public Staff(string name, int age, string post, int salary)
+        {
+            _name = name;
+            _age = age;
+            _post = post;
+            _salary = salary;
+            _name = name;
+        }
+        public void print()
+        {
+            Console.WriteLine("Имя : \t{0}\t" +
+                "Возраст : {1}\t" +
+                "должность : \t{2}\t\t"+
+                "Зарплата : \t{3}\t", _name,_age,_post,_salary);
+
+        }
+        public String Name { get { return _name; } set { _name = value; } }// дать взять имя
+        public int Age { get { return _age; } set { _age = value; } }       // дать взять возраст
+        public String Post { get { return _post; } set { _post = value; } } // дать взять должность
+        public int Salary { get { return _salary; } set { _salary = value; } } // дать взять зарплату
     }
+   
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Example example = new Example();
-            Write("Введите целое число:");
-            example.Num = int.Parse(ReadLine()); // set
-            
-        Write("Вы ввели:");
-            WriteLine(example.Num); // get
+            Staff one = new Staff();
+            Write("Введите имя:\t");
+            one.Name = Console.ReadLine(); // set
+            Write("Введите возраст:\t");
+            one.Age = int.Parse(ReadLine()); // set
+            Write("Введите должность:\t");
+            one.Post = ReadLine(); // set
+            Write("Введите зарплату:\t");
+            one.Salary = int.Parse(ReadLine()); // set
+            one.print();
         }
     }
 }
+
